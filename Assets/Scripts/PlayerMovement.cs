a8fd749eb8309fbb8644c3362593951bf883267f
Giuliocasadio <giuliocasa@hotmail.com>
2021-05-01 02:42:54 +0200

    Movimento migliorato

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -21,36 +21,19 @@ public class PlayerMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (joy.Horizontal >= .2f)
-        {
-            horizontalMove = speed;
-        } 
-        else if (joy.Horizontal <= -.2f)
-        {
-            horizontalMove = -speed;
-        }  
-        else 
-        {
-            horizontalMove = 0;
-        }
-
-        if (joy.Vertical >= .2f)
-        {
-            verticalMove = speed;
-        }
-        else if (joy.Vertical <= -.2f)
-        {
-            verticalMove = -speed;
-        } 
+        if (joy.Horizontal >= .2f || joy.Horizontal <= .2f)
+            horizontalMove = speed * joy.Horizontal;
         else
-        {
-            verticalMove = 0;
-        }
+            horizontalMove = 0;
 
+        if (joy.Vertical >= .2f || joy.Vertical <= .2f)
+            verticalMove = speed * joy.Vertical;
+        else
+            verticalMove = 0;
     }
 
     private void FixedUpdate()
     {
-        rb.MovePosition(new Vector3(rb.position.x + horizontalMove * speed * Time.deltaTime, 0, rb.position.z + verticalMove * speed * Time.deltaTime));
+        rb.MovePosition(new Vector3(rb.position.x + horizontalMove * speed * Time.fixedDeltaTime, 0, rb.position.z + verticalMove * speed * Time.fixedDeltaTime));
     }
 }
