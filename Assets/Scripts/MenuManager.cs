28182fdb2f41c76802ba82da2ad0ef40edd57da6
Giulio <giuliocasa@hotmail.com>
2021-05-14 10:46:31 +0200

    Introduzione Scene manager e primi elementi di ui

---
+++ Assets/Scripts/MenuManager.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using TMPro;
+using UnityEngine.UI;
+
+public class MenuManager : MonoBehaviour
+{
+    //private int bestScore;
+
+    //public TextMeshProUGUI best;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //bestScore = PlayerPrefs.GetInt("score", 0);
+        //best.text = "Record: " + bestScore;
+        Cursor.lockState = CursorLockMode.Confined;
+    }
+
+    public void PlayLevel()
+    {
+        SceneManager.LoadScene("Scenes/Level1");
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,15 +1,30 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using TMPro;
+using UnityEngine.UI;
 
 public class PlayerMovement : MonoBehaviour
 {
+    [Header("Game")]
     public Joystick joy;
     public float speed = 20f;
-    private float touchSpeed = 5;
 
+    [Header("Hud")]
+    //public TextMeshProUGUI punteggio;
+
+    [Header("GameOver")]
+    //public TextMeshProUGUI nuovoPunteggio;
+    //public TextMeshProUGUI record;
+
+    [Header("Pause")]
+    //public TextMeshProUGUI nuovoPunteggioPausa;
+    //public TextMeshProUGUI recordPausa;
+
+    private float touchSpeed = 5;
     private Rigidbody rb;
-    private float horizontalMove = 0f, verticalMove =0f;
+    private float horizontalMove = 0f, verticalMove = 0f;
 
     // Start is called before the first frame update
     void Start()
@@ -36,4 +51,51 @@ public class PlayerMovement : MonoBehaviour
     {
         rb.MovePosition(new Vector3(rb.position.x + horizontalMove * speed * Time.fixedDeltaTime, 0, rb.position.z + verticalMove * speed * Time.fixedDeltaTime));
     }
+
+    public void Pause()
+    {
+        //pause.SetActive(true);
+        //hud.SetActive(false);
+        //Cursor.lockState = CursorLockMode.Confined;
+        //Time.timeScale = 0;
+        //gameIsPaused = true;
+
+        //recordPausa.text = "Record: " + PlayerPrefs.GetInt("score", 0);
+        //nuovoPunteggioPausa.text = "Punteggio: " + punteggioCounter;
+    }
+
+    public void Resume()
+    {
+        //pause.SetActive(false);
+        //hud.SetActive(true);
+        //Cursor.lockState = CursorLockMode.Locked;
+        //Time.timeScale = 1;
+        //gameIsPaused = false;
+    }
+
+    public void GameOver()
+    {
+        //gameOver.SetActive(true);
+        //hud.SetActive(false);
+        //Cursor.lockState = CursorLockMode.Confined;
+        //Time.timeScale = 0;
+        //record.text = "Record: " + PlayerPrefs.GetInt("score", 0);
+        //nuovoPunteggio.text = "Punteggio: " + punteggioCounter;
+    }
+
+    public void StartScene()
+    {
+        SceneManager.LoadScene("Scenes/FirstMenu");
+    }
+
+    //public void RetartScene()
+    //{
+    //    SceneManager.LoadScene("Scenes/MainScene");
+    //}
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+
 }
